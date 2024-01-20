3b8e394b3c39ffc96914deb83f2f0e407f517a24
Vojta2 <nomovl@seznam.cz>
2024-01-20 12:27:09 +0100

    Přidejte soubory projektu.

---
+++ editor1/editor1.cpp
@@ -0,0 +1,16 @@
+﻿// editor1.cpp: Definuje vstupní bod pro aplikaci.
+//
+#include <QtWidgets/QApplication>
+
+// Include header files for application components.
+// ...
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    // Set up and show widgets.
+    // ...
+
+    return app.exec();
+}
---
+++ editor1/example_cpp_file.cpp
@@ -0,0 +1,5 @@
+#include "example_cpp_file.h"
+
+void MyInt::hello() {
+	std::cout << "Hello, world!" << std::endl;
+}
