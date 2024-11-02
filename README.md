f212dc683763eec69bc9c27e2effdf966809d274
Vojtěch Novotný <vojtech.novotny@netrex.cz>
2024-11-02 01:44:25 +0100

    First working Linux build?

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO tohle vsechno, jen sepisuju myslneky xd
+
+1. ve VSCODE nastaveni najdi C/C++: Edit configurations (UI), tam include path, přidej tam něco jako /usr/local/qt6/**
+2. Někam si pullni vcpkg repozitář a v compile.sh k němu nastav cestu (TODO parametr)
+3. 
--- editor1/editor1.cpp
+++ editor1/editor1.cpp
@@ -1,16 +1,14 @@
-﻿// editor1.cpp: Definuje vstupní bod pro aplikaci.
-//
-#include <QtWidgets/QApplication>
-
-// Include header files for application components.
-// ...
+﻿#include <QtWidgets/QApplication>
+#include <QMainWindow>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    // Set up and show widgets.
-    // ...
+    QMainWindow mainWindow;
+    mainWindow.setWindowTitle("Hello, Qt!");
+    mainWindow.resize(400, 300);
+    mainWindow.show();
 
     return app.exec();
 }
