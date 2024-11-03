cb7fbc2f6c319188a4e86ece5a9d24c64a7f7cce
Vojtěch Novotný <vojtech.novotny@netrex.cz>
2024-11-03 20:40:03 +0100

    compile changes

--- compile.sh
+++ compile.sh
@@ -1,15 +1,15 @@
 
-VCPKG_ROOT="C:/vcpkg"
-TRIPLET="x64-windows" #of course change this to linux (or delete)
+VCPKG_ROOT="/home/vojtechnovotny/Desktop/vcpkg" # TODO nice printing if not found
+TRIPLET="x64-linux" #of course change this to linux/windows (or delete)
 
 CMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake";
 
 # TODO ne vzdycky se to musi instalovat
 $VCPKG_ROOT/bootstrap-vcpkg.sh;
 export PATH=$PATH:$VCPKG_ROOT;
-vcpkg install fmt --triplet=$TRIPLET
-vcpkg install gtkmm --triplet=$TRIPLET
-vcpkg install libsigcpp --triplet=$TRIPLET
-vcpkg install pkgconf --triplet=$TRIPLET
+$VCPKG_ROOT/vcpkg install fmt --triplet=$TRIPLET
+$VCPKG_ROOT/vcpkg install gtkmm --triplet=$TRIPLET
+$VCPKG_ROOT/vcpkg install libsigcpp --triplet=$TRIPLET
+$VCPKG_ROOT/vcpkg install pkgconf --triplet=$TRIPLET
 
 cmake ./CMakeLists.txt -DVCPKG_ROOT=$VCPKG_ROOT -DTRIPLET=$TRIPLET -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
